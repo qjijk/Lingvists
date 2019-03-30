6384f4a7c4c7f3af31baa513b3f16ccc5295b232
qgwFalcon <qgw1@outlook.com>
2019-03-30 14:33:16 +0800

    Initial commit

---
+++ app/src/main/java/com/url/msi/lingvist/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.url.msi.lingvist;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.king.view.circleprogressview.CircleProgressView;
+
+
+public class HomeFragment extends Fragment {
+
+    TextView textView;
+    private CircleProgressView cpv;
+    private int[] mShaderColors = new int[]{0xFF4FEAAC,0xFFA8DD51,0xFFE8D30F,0xFFA8DD51,0xFF4FEAAC};
+
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        // Inflate the layout for this fragment
+        textView = view.findViewById(R.id.t1);
+        textView.setText("测试");
+        cpv = view.findViewById(R.id.cpv);
+        cpv.setShowTick(false);
+
+        cpv.setProgressColor(mShaderColors);
+        cpv.showAnimation(70,1500);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/url/msi/lingvist/NotificationFragment.java
@@ -0,0 +1,43 @@
+package com.url.msi.lingvist;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.king.view.circleprogressview.CircleProgressView;
+//import com.android.support:appcompat-v7:28.0.0;
+
+
+public class NotificationFragment extends Fragment {
+
+    private CircleProgressView cpv;
+
+
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+
+
+
+
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_notification, container, false);
+        return view;
+
+        //circleProgressView.showAnimation(80,3000);
+    }
+
+}
