0ea05afed374a518c2967fe16418f6dd58000e33
qgwFalcon <qgw1@outlook.com>
2019-04-02 19:46:21 +0800

    增加了SQLite数据库读取。但未导入数据库。

--- app/src/main/java/com/url/msi/lingvist/MainActivity.java
+++ app/src/main/java/com/url/msi/lingvist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.url.msi.lingvist;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,12 +22,24 @@ import com.king.view.circleprogressview.CircleProgressView;
 
 public class MainActivity extends AppCompatActivity {
     private CircleProgressView cpv;
+
+    SQLiteDatabase db;
+    AssetsDatabaseManager mg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
         setContentView(R.layout.activity_main);
+
+        // 初始化，只需要调用一次
+        AssetsDatabaseManager.initManager(getApplication());
+        // 获取管理对象，因为数据库需要通过管理对象才能够获取
+        mg = AssetsDatabaseManager.getManager();
+        // 通过管理对象获取数据库
+        db = mg.getDatabase("Lingvist.db");
+
 
 
         BottomNavigationView bottomNav = findViewById(R.id.main_navigation_bar);
