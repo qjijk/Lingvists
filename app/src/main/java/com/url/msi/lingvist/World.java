1f66bd28aad47bb6841d61598c72351cd999fb65
qgwFalcon <qgw1@outlook.com>
2019-04-11 21:31:44 +0800

    优化了若干东西。但是不重要，所以就不列出了

--- app/src/main/java/com/url/msi/lingvist/World.java
+++ app/src/main/java/com/url/msi/lingvist/World.java
@@ -7,8 +7,11 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -27,9 +30,12 @@ public class World extends AppCompatActivity {
 
     ArrayList<Word> wordArrayList;
     String key, sent, tra, senttra;
-    TextView sentText, traText, senttraText;
+    TextView sentText, traText, senttraText, leandText;
     EditText inputText;
     Handler handler = null;
+    int cont = 0;
+
+    Button button ;
 
     SQLiteDatabase db;
     AssetsDatabaseManager mg;
@@ -49,28 +55,26 @@ public class World extends AppCompatActivity {
         db = mg.getDatabase("Lingvist.db");
 
         setWordArrayList();
+        senttraText = findViewById(R.id.wordTranslation);
+        sentText = findViewById(R.id.sentence);
+        traText = findViewById(R.id.sentenceTranslation);
+        leandText = findViewById(R.id.learnedNum);
+        button = findViewById(R.id.submit);
 
-
-
-        senttraText = (TextView) findViewById(R.id.wordTranslation);
-        sentText = (TextView) findViewById(R.id.sentence);
-        traText = (TextView) findViewById(R.id.sentenceTranslation);
-        JS();
-
-        /*handler = new Handler(){
-
-            public void handleMassage(Message msg){
-                Log.i("wsssss","qssssq");
-                super.handleMessage(msg);
-                String str = String.valueOf(msg.obj);
-                sentText.setText(str);
-                Log.i("wqwqwq","qweweq");
+        leandText.setText("0");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeWord();
+                cont++;
+                String cc = String.valueOf(cont);
+                leandText.setText(cc);
+                //Toast.makeText(World.this,"测试",Toast.LENGTH_LONG).show();
             }
-        };*/
+        });
+
+        changeWord();
     }
-
-
-
 
     private void setWordArrayList() {
         Cursor cursor = db.rawQuery("select * from s123", null);
@@ -86,27 +90,38 @@ public class World extends AppCompatActivity {
     }
 
 
+
     // URL url = new URL(http://dict-co.iciba.com/api/dictionary.php?w=go&key=6810AFA1CA6CAA1DC7C1CFA32F41DD4A);
     //金山API获取xml
 
-    public void JS() {
-
+    private void changeWord()
+    {
         int size = wordArrayList.size();
-        int position = (int) Math.round(random() * (size - 1));
+        int position = (int) Math.round(random() * (size - 1));//暂时为随机获取单词，等想到到方法则换成按照词频
         Word word = wordArrayList.get(position);
         key = word.getWord();//获取单词
         int a = word.getCp();//获取词频
+        if (a != 1) {
+            changeWord();//影响性能。需要更换数据存储结构
+        }else {
+            JS(key);
+        }
+    }
 
+
+
+
+    public void JS(final String key) {
         new Thread(new Runnable() {
-            String d1;
-            String d2;
-            String d3 = null;
+            String d1 = null;//句子
+            String d2 = null;//翻译
+            String d3 = null;//意思
             @Override
             public void run() {
                 Log.i("w", key);
                 try {
                     //URL url = new URL("http://dict-co.iciba.com/api/dictionary.php?w=go&key=6810AFA1CA6CAA1DC7C1CFA32F41DD4A");
-                    Document document = Jsoup.connect("http://dict-co.iciba.com/api/dictionary.php?w="+key+"&key=6810AFA1CA6CAA1DC7C1CFA32F41DD4A").get();
+                    Document document = Jsoup.connect("http://dict-co.iciba.com/api/dictionary.php?w="+key+"&key=6810AFA1CA6CAA1DC7C1CFA32F41DD4A").get();//获取xml,key为所查询的单词
                     Elements elements = document.select("dict");
                     Elements e2 = elements.select("sent");
                     //每个词只获取第一个例句
@@ -116,27 +131,24 @@ public class World extends AppCompatActivity {
                         Log.i("E",d1);
                         Log.i("C",d2);
                     }
-                    Elements e3 = elements.select("pos");
-                    Elements e4 = elements.select("acceptation");
+                    Elements e3 = elements.select("pos");//pos为词性
+                    Elements e4 = elements.select("acceptation");//acceptation为意思
                     for (int i = 0; i < e3.size() && i < e4.size() && i < 2; i++)//最多只给出两个意思
                     {
-                        if(d3 != null)
-                        {
-                            d3 = d3 +"\n"+ e3.get(i).select("pos").text() + e4.get(i).select("acceptation").text();
+                        if(d3 != null) {
+                            d3 = d3 +"\n"+ e3.get(i).select("pos").text() + e4.get(i).select("acceptation").text();//使其分行
                         }else{
                             d3 = e3.get(i).select("pos").text() + e4.get(i).select("acceptation").text();
-
                         }
-
                     }
                     Log.i("d3",d3);
-
+                    //更新UI的线程
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            sentText.setText(d1);
-                            traText.setText(d2);
-                            senttraText.setText(d3);
+                            sentText.setText(d1);//更新句子
+                            traText.setText(d2);//更新句子翻译
+                            senttraText.setText(d3);//更新单词意思
                         }
                     });
 
